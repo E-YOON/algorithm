8c73e0436ec55784b241ce375509bb88b5303361
E-YOON <yunjeongy930@gmail.com>
2023-03-21 13:41:30 +0900

    2023-03-21

---
+++ bronze4/b3765.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class b3765 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()) {
+            String str = scan.nextLine();
+            System.out.println(str);
+        }
+        scan.close();
+    }
+}
