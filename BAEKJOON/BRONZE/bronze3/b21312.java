bc4fdb08b816f0bf5677091f5513a755565bd6c4
E-YOON <yunjeongy930@gmail.com>
2023-04-05 15:35:04 +0900

    2023-04-05

---
+++ BAEKJOON/BRONZE/bronze3/b21312.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b21312 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[3];
+        boolean odd = false;
+        int answer = 1;
+
+        for(int i=0; i<3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(arr[i] % 2 != 0) {
+                answer *= arr[i];
+                odd = true;
+            }
+        }
+
+        System.out.println(odd ? answer : arr[0] * arr[1] * arr[2]);
+        
+    }
+}
