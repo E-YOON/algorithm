92527f09ff0639c185fabbdde6d8d8140c0b39e1
E-YOON <yunjeongy930@gmail.com>
2023-05-27 18:09:59 +0900

    2023-05-27

---
+++ BAEKJOON/BRONZE/bronze2/b2231.java
@@ -0,0 +1,30 @@
+// 분해합
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        for(int i=0; i<N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while(num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum + i == N) {
+                answer = i;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
