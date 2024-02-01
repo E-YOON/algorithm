7a73bac89e6195a25c7a3599897921711c88533e
E-YOON <yunjeongy930@gmail.com>
2024-02-01 15:04:20 +0900

    수 정렬하기 2

--- BAEKJOON/SILVER/silver5/s2751.java
+++ BAEKJOON/SILVER/silver5/s2751.java
@@ -1,29 +1,73 @@
 // 수 정렬하기 2
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.OutputStreamWriter;
 
 public class s2751 {
+    public static int[] arr, tmp;
+    public static long result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
+        arr = new int[N+1];
+        tmp = new int[N+1];
 
-        for(int i=0; i<N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+        for(int i=1; i<=N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
         
-        Collections.sort(list);
+        mergeSort(1, N);
 
-        for(int num : list) {
-            sb.append(num).append('\n');
+        for(int i=1; i<=N; i++) {
+            bw.write(arr[i] + "\n");
         }
 
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
+    }
+
+    private static void mergeSort(int s, int e) {
+        if(e-s < 1) return;
+
+        int m = s + (e-s) / 2;
+
+        mergeSort(s, m);
+        mergeSort(m+1, e);
+
+        for(int i=s; i<=e; i++) {
+            tmp[i] = arr[i];
+        }
+
+        int k = s;
+        int index1 = s;
+        int index2 = m+1;
+
+        while(index1 <= m && index2 <= e) {
+            if(tmp[index1] > tmp[index2]) {
+                arr[k] = tmp[index2];
+                k++;
+                index2++;
+            } else {
+                arr[k] = tmp[index1];
+                k++;
+                index1++;
+            }
+        }
+
+        while(index1 <= m) {
+            arr[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+        while (index2 <= e) {
+            arr[k] = tmp[index2];
+            k++;
+            index2++;
+        }
     }
 }
